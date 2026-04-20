b9decf7fabd93e16739eed1072cd6e54bcf315f7
JkhTV <86148752+NewLandTV@users.noreply.github.com>
2026-04-21 06:29:58 +0900

    [Bronze III] Title: Sequences, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5101. Sequences/Sequences.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	
+	while (1)
+	{
+		scanf("%d %d %d", &a, &b, &c);
+		
+		if (a == 0 && b == 0 && c == 0)
+		{
+			break;
+		}
+		
+		if ((c - a) % b != 0 || (c - a) / b < 0)
+		{
+			printf("X\n");
+		}
+		else
+		{
+			printf("%d\n", (c - a) / b + 1);
+		}
+	}
+	
+	return 0;
+}
